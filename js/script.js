4e1dc81dd6ec25befd04d44ef16746a56824c0c0
Denys iElkin <hvoarang@gmail.com>
2021-12-03 21:53:14 +0200

    delete copy file style.css

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let numberOfFilms = +prompt('Сколько фильмов вы можете посмотреть', '');
+
+const personalMovieDB + {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: (),
+};
